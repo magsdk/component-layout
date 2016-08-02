03bc7abcac3f7f41257208dd29f4d6af0fbd78e6
Yaroslav <y.surilov@infomir.com>
2016-08-02 11:35:48 +0300

    add some sass, compile to css, fix config

---
+++ index.js
@@ -0,0 +1,209 @@
+/**
+ * @license The MIT License (MIT)
+ * @copyright Stanislav Kalashnik <darkpark.main@gmail.com>
+ */
+
+/* eslint no-path-concat: 0 */
+
+'use strict';
+
+var Component = require('stb-component'),
+    rc        = require('stb-rc');
+
+/**
+ * Layout component implementation
+ *
+ * @constructor
+ * @extends Component
+ *
+ * @param {Object}   [config={}]          init parameters (all inherited from the parent)
+ * @param {Array} [config.data] array of items to add to layout
+ *
+ * @example
+ * var Layout = require ('../stb/ui/layout'),
+ *     layout = new Layout({
+ *         data:[
+ *             'Some text'
+ *             {
+ *                 className: 'icon star',
+ *             },
+ *             {
+ *                 value: new Input()
+ *             },
+ *             new Button({'value:'Ok'})
+ *             ]
+ *         });
+ */
+function Layout ( config ) {
+
+    // sanitize
+    config = config || {};
+
+    /**
+     * Index of focused child component
+     * @type {number}
+     */
+    this.focusIndex = 0;
+
+    if ( DEVELOP ) {
+        if ( typeof config !== 'object' ) { throw new Error(__filename + ': wrong config type'); }
+        // init parameters checks
+        if ( config.className && typeof config.className !== 'string' ) { throw new Error(__filename + ': wrong or empty config.className'); }
+    }
+
+    config.className = 'layout ' + (config.className || '');
+
+    /**
+     * Component data
+     * @type {Array}
+     */
+    this.data = [];
+
+    Component.call(this, config);
+
+    this.init(config);
+
+    // add listener to move focus between children
+    this.addListener('keydown', function ( event ) {
+        switch ( event.code ) {
+            case rc.codes.right:
+                if ( this.children.length && this.focusIndex < this.children.length - 1 ) {
+                    this.children[++this.focusIndex].focus();
+                }
+                break;
+            case rc.codes.left:
+                if ( this.children.length && this.focusIndex > 0 ) {
+                    this.children[--this.focusIndex].focus();
+                }
+                break;
+            case rc.codes.back:
+                // focus parent
+                this.parent.focus();
+
+                // focus parent focused item if parent is layout list
+                if ( this.parent &&  this.$parentItem ) {
+                    this.parent.focusItem(this.$parentItem);
+                }
+                break;
+        }
+    });
+
+}
+
+
+Layout.prototype = Object.create(Component.prototype);
+Layout.prototype.constructor = Layout;
+
+
+/**
+ * Make all the data items identical.
+ * Wrap to objects if necessary.
+ *
+ * @param {Array} data incoming array
+ * @return {Array} reworked incoming data
+ */
+function normalize ( data ) {
+    var index, item;
+
+    if ( DEVELOP ) {
+        if ( arguments.length !== 1 ) { throw new Error(__filename + ': wrong arguments number'); }
+        if ( !Array.isArray(data) ) { throw new Error(__filename + ': wrong data type'); }
+    }
+
+    // rows
+    for ( index = 0; index < data.length; index++ ) {
+        // cell value
+        item = data[index];
+        // plain text
+        if ( typeof item !== 'object' ) {
+            // wrap with defaults
+            data[index] = {
+                value: data[index],
+                wrap : true
+            };
+        } else {
+            // HTML element or component
+            if ( item instanceof Component || item instanceof HTMLElement ) {
+                data[index] = {
+                    value: item,
+                    wrap : false
+                };
+            } else {
+                data[index].wrap = true;
+            }
+        }
+    }
+    return data;
+}
+
+
+/**
+ * Init or re-init of the component inner structures and HTML.
+ *
+ * @param {Object} config init parameters (subset of constructor config params)
+ */
+Layout.prototype.init = function ( config ) {
+    var self = this,
+        data = normalize(config.data),
+        item, $wrapper, index;
+
+    // clear element if reinit
+    while (this.$node.firstChild) {
+        this.$node.removeChild(this.$node.firstChild);
+    }
+
+    this.data = data;
+
+    for ( index = 0; index < data.length; index++ ) {
+        item = data[index];
+        // plain text
+        if ( typeof item.value === 'string' ) {
+            $wrapper = document.createElement('div');
+            $wrapper.textContent = item.value;
+            if ( item.className ) { $wrapper.className = item.className; }
+            this.$node.appendChild($wrapper);
+        } else if ( item.value instanceof HTMLElement ) {
+            // HTML Element
+
+            // if with wrapper
+            if ( item.wrap ) {
+                $wrapper = document.createElement('div');
+                if ( item.className ) { $wrapper.className = item.className; }
+                $wrapper.appendChild(item.value);
+                this.$node.appendChild($wrapper);
+            } else {
+                // without wrapper
+                this.$node.appendChild(item.value);
+            }
+        } else if ( item.value instanceof Component ) {
+            // component
+            // force propagate events
+            item.value.propagate = true;
+
+            // set index to current component
+            item.value.index = this.children.length;
+
+            // change layout focus index if click component
+            item.value.addListener('click', function () {
+                self.focusIndex = this.index;
+            });
+
+            // append component
+            if ( item.wrap ) {
+                // with wrapper
+                $wrapper = document.createElement('div');
+                if ( item.className ) { $wrapper.className = item.className; }
+                $wrapper.appendChild(item.value.$node);
+                this.$node.appendChild($wrapper);
+                this.children.push(item.value);
+                item.value.parent = this;
+            } else {
+                // without wrapper
+                this.add(item.value);
+            }
+        }
+    }
+};
+
+
+module.exports = Layout;
---
+++ readme.md
@@ -0,0 +1,41 @@
+Layout component
+================
+
+[![NPM version](https://img.shields.io/npm/v/mag-component-layout.svg?style=flat-square)](https://www.npmjs.com/package/mag-component-layout)
+[![Dependencies Status](https://img.shields.io/david/spasdk/component-layout.svg?style=flat-square)](https://david-dm.org/spasdk/component-layout)
+[![Gitter](https://img.shields.io/badge/gitter-join%20chat-blue.svg?style=flat-square)](https://gitter.im/DarkPark/spasdk)
+
+
+Layout list is a component to build user interface, an instance of [Component](https://github.com/spasdk/component) module.
+
+
+## Installation ##
+
+```bash
+npm install mag-component-layout
+```
+
+
+## Usage ##
+
+Add the singleton to the scope:
+
+```js
+var list = require('mag-component-layout');
+```
+
+
+## Development mode ##
+
+> There is a global var `DEVELOP` which activates additional consistency checks and protection logic not available in release mode.
+
+
+## Contribution ##
+
+If you have any problem or suggestion please open an issue [here](https://github.com/spasdk/component-layout/issues).
+Pull requests are welcomed with respect to the [JavaScript Code Style](https://github.com/DarkPark/jscs).
+
+
+## License ##
+
+`mag-component-layout` is released under the [MIT License](license.md).
